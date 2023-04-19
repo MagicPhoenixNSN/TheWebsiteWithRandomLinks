41224b79e2a12e4c21972c615c7bcbd5d7d4ecb2
Alex DeSilva <adesilva@newschoolva.org>
2023-04-19 10:58:10 -0400

    First Commit

--- CodeForClick.js
+++ CodeForClick.js
@@ -1,9 +1,22 @@
 const btn = document.getElementById('btn');
+const upgradeBtn = document.getElementById('upgradebtn')
 let numberClicks = 0;
 let clickMutlipleir = 1
+let multipleirCost = 10
 
 btn.addEventListener('click', function handleClick() {
     numberClicks = numberClicks + clickMutlipleir
-    btn.textContent = `Button clicked ${numberClicks}`;
+    btn.textContent = `Button Clicked ${numberClicks}`;
   });
 
+upgradeBtn.addEventListener('click', function handleClick() {
+    if (numberClicks >= multipleirCost){
+        clickMutlipleir = clickMutlipleir + 1
+        multipleirCost = multipleirCost * 1.5
+        multipleirCost = Math.round(multipleirCost)
+        numberClicks = 0
+        btn.textContent = `Button Clicked ${numberClicks}`
+        upgradeBtn.textContent = `Upgrade for : ${multipleirCost}`
+    }
+
+})
